7de6665f5705e7f6e310cbe68b240f05e6ccc78c
Brunojoe1995 <137604361+Bruno-bot-stack@users.noreply.github.com>
2025-02-26 17:07:41 +0530

    Initialize main638434659284512031sync_temp

---
+++ docs/snippets/csharp/VS_Snippets_CodeAnalysis/FxCop.Usage.UseParams/cs/FxCop.Usage.UseParams.cs
@@ -0,0 +1,30 @@
+//<Snippet1>
+using System;
+
+[assembly: CLSCompliant(true)]
+namespace UsageLibrary
+{
+    public class UseParams 
+    {
+        // This method violates the rule.
+        [CLSCompliant(false)]
+        public void VariableArguments(__arglist) 
+        { 
+            ArgIterator argumentIterator = new ArgIterator(__arglist);
+            while (argumentIterator.GetRemainingCount() > 0)
+            { 
+                Console.WriteLine(__refvalue(argumentIterator.GetNextArg(), string));
+            } 
+        }
+
+        // This method satisfies the rule.
+        public void VariableArguments(params string[] wordList)
+        { 
+            for(int i = 0; i < wordList.Length; i++) 
+            { 
+                Console.WriteLine(wordList[i]);
+            } 
+        }
+    }
+}
+//</Snippet1>
